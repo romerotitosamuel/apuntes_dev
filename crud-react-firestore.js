cd3fe7e04b6fdf3688c6572cecd3ff3b85343ccf
tsamuel1289 <52178585+tsamuel1289@users.noreply.github.com>
2021-05-08 07:45:57 -0400

    UPDATE

--- crud-react-firestore.js
+++ crud-react-firestore.js
@@ -1,4 +1,13 @@
 FAZT Web - CRUD React Firebase
 
-14:50 emmet on VS code configuration
-23:57 Connect with materialize
+Solucion npm start
+https://bertofern.wordpress.com/2019/01/08/solucion-node-js-npm-no-reconocido-como-comando-interno-o-externo/
+
+14:50 "emmet" on VS code configuration
+23:57 Connect with materialize 
+      https://materializecss.com/icons.html
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+              <i class="material-icons">add</i> //add is de name icon
+27:43 Create a button and begin to programming functions
+
+//QUEDE EN EL MINUTO 27:43
