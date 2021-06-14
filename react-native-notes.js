6e4a9b5ca01d15d3ecf48d3799883b299b12925d
tsamuel1289 <52178585+tsamuel1289@users.noreply.github.com>
2021-06-14 14:25:35 -0400

    update

--- react-native-notes.js
+++ react-native-notes.js
@@ -7,4 +7,8 @@ voy por 55:29
 
 Native CRUD https://www.youtube.com/watch?v=VE7J0SA1PRQ&t=1199s
 
+Para navegacion
+reactnavigation.org // se instala el npm, y tb la dependencia para expo
+//tb se debe instalar reactnavigatin stack
 
+14:00 me quede, empezo navegacion
